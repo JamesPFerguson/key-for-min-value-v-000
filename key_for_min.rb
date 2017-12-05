44297042df9fddacb9f1a7ec09b783588ee10c9e
James Ferguson <ncjamesferguson@gmail.com>
2017-12-05 17:09:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_key = nil
   low_val = nil
   name_hash.each do |key, val|
-    if low_val == nil || low_val < val
+    if low_val == nil || val < low_val
       low_val = val
       low_key = key
     end
